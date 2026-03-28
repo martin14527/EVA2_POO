a013f334523479fba1b88b8908d0e5e46e008c27
martin14527 <guillermovallejoloya@gmail.com>
2026-03-27 22:59:06 -0600

    Add files via upload

---
+++ EVA2_12_HERENCIA INTERFAZ/src/eva2_12_herencia/interfaz/EVA2_12_HERENCIAINTERFAZ.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_12_herencia.interfaz;
+
+/**
+ *
+ * @author anoni
+ */
+public class EVA2_12_HERENCIAINTERFAZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+interface prueba{
+void mostrarmensaje();}
+
+
+interface otraprueba{
+void mostrarsaludo(String mensaje);
+
+class demostratcion() implements otraprueba{}
+
+
+
+
+}
