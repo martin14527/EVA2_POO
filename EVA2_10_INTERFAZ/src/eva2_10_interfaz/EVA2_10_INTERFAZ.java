ae47af2bd315ec77276531aa52b4da85d39e9ac2
martin14527 <guillermovallejoloya@gmail.com>
2026-04-11 23:32:00 -0600

    Refactor EVA2_10_INTERFAZ and implement Persona class
    Refactor class and interface names to follow Java naming conventions. Implement Persona class and its method to display a message.

--- EVA2_10_INTERFAZ/src/eva2_10_interfaz/EVA2_10_INTERFAZ.java
+++ EVA2_10_INTERFAZ/src/eva2_10_interfaz/EVA2_10_INTERFAZ.java
@@ -1,13 +1,34 @@
+package eva2_10_interfaz;
 
 public class EVA2_10_INTERFAZ {
+
     public static void main(String[] args) {
 
-        persona p = new persona("Guillermo", "Vallejo", 18);
-        p.mostrarmensaje();
-        
+        Persona p = new Persona("guillermo", "vallejo", 18);
+        p.mostrarMensaje();
     }
 }
-interface mensaje{
-public void mostrarmesnaje();
-System.out.println("");
+
+interface Mensaje {
+    public void mostrarMensaje();
+}
+
+
+class Persona implements Mensaje {
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    @Override
+    public void mostrarMensaje() {
+        System.out.println("Hola, soy " + nombre + " " + apellido +
+                " y tengo " + edad + " años.");
+    }
 }
