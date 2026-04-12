97944d7f6731b4bc9cdc076e3b8b63e0f16e9f82
martin14527 <guillermovallejoloya@gmail.com>
2026-04-11 23:32:27 -0600

    Update persona.java

--- EVA2_10_INTERFAZ/src/eva2_10_interfaz/persona.java
+++ EVA2_10_INTERFAZ/src/eva2_10_interfaz/persona.java
@@ -1,21 +1,20 @@
 package eva2_10_interfaz;
 
-public class persona implements mensaje {
+public class persona implements Mensaje {
 
     private String nombre;
     private String apellido;
     private int edad;
 
-   
     public persona(String nombre, String apellido, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
     }
 
-  
-    public void mostrarmensaje() {
-        System.out.println("Hola, soy " + nombre + " " + apellido + 
-                           " y tengo " + edad + " años.");
+    @Override
+    public void mostrarMensaje() {
+        System.out.println("hola, soy " + nombre + " " + apellido +
+                " y tengo " + edad + " años.");
     }
 }
